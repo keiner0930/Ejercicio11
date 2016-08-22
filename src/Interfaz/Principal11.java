2e8808f7f26fd480ddbcdb4618e4cdc3914173c1
“keiner0930” <keiner.arrieta0930@hotmail.com>
2016-08-22 11:26:08 -0500

    Interfaz Creada

--- src/Interfaz/Principal11.java
+++ src/Interfaz/Principal11.java
@@ -28,10 +28,65 @@ public class Principal11 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtMonto = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtGinecologia = new javax.swing.JTextField();
+        txtTraumatologia = new javax.swing.JTextField();
+        txtPediatria = new javax.swing.JTextField();
+        cmdCalcular = new javax.swing.JButton();
+        cmdBorra = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Traditional Arabic", 2, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(204, 0, 0));
+        jLabel1.setText("Hospital");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 10, 150, -1));
+
+        jLabel2.setFont(new java.awt.Font("Traditional Arabic", 2, 24)); // NOI18N
+        jLabel2.setText("Monto Presupuestal");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 70, 220, -1));
+        jPanel1.add(txtMonto, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 110, 110, 30));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 160, 400, 10));
+
+        jLabel3.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel3.setText("Ginecologia Recibira:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 180, -1));
+
+        jLabel4.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel4.setText("Traumatologia Recibira :");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, 210, -1));
+
+        jLabel5.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel5.setText("Pediatria Recibira :");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 260, -1, -1));
+
+        txtGinecologia.setEditable(false);
+        txtGinecologia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtGinecologiaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtGinecologia, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 170, 90, 30));
+
+        txtTraumatologia.setEditable(false);
+        jPanel1.add(txtTraumatologia, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 210, 90, 30));
+
+        txtPediatria.setEditable(false);
+        jPanel1.add(txtPediatria, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 250, 90, 30));
+
+        cmdCalcular.setText("Calcular");
+        jPanel1.add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 310, -1, -1));
+
+        cmdBorra.setText("Borrar");
+        jPanel1.add(cmdBorra, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 310, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -41,11 +96,15 @@ public class Principal11 extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtGinecologiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtGinecologiaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtGinecologiaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -83,6 +142,18 @@ public class Principal11 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdBorra;
+    private javax.swing.JButton cmdCalcular;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTextField txtGinecologia;
+    private javax.swing.JTextField txtMonto;
+    private javax.swing.JTextField txtPediatria;
+    private javax.swing.JTextField txtTraumatologia;
     // End of variables declaration//GEN-END:variables
 }
